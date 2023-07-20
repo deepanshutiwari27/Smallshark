cb9e4fa2af08302a06db934340a0f559dcd5f30a
Nemesis <83503290+Nemesis0U@users.noreply.github.com>
2023-07-20 18:45:56 +0100

    Update Smallshark.py

--- Smallshark/Smallshark.py
+++ Smallshark/Smallshark.py
@@ -30,7 +30,7 @@ print(Fore.LIGHTBLUE_EX + r'''                                 ,-
                                    ""            "            `-._) ''')
 ascii_banner = pyfiglet.figlet_format("Smallshark")
 print(Fore.LIGHTWHITE_EX + ascii_banner)
-print("\nDeanonymization & getting shell through undetectable bait files.\n\n")
+print("\nDeanonymization of TOR Users & getting reverse shell through antivirus evading fake image files.\n\n")
 polymorphic = RandomData()
 polymorphic2 = RandomData()
 polymorphic3 = RandomData()
